6064b8f26966a8a9671f98b40d760428c731d625
Laurent Michel <laurent.michel@cognex.com>
2023-02-07 18:16:38 +0100

    update hasura installation scripts

--- hasura/install_hasura.sh
+++ hasura/install_hasura.sh
@@ -1,36 +1,27 @@
 #! /bin/sh
 
-if [ "$#" -ne "12" ] ; then
-  echo "Usage: $0 <baseUrl> <postgresReleaseName> <postgresNamespace> <iamReleaseName> <iamNamespace> <postgresqlUsername> <postgresqlPassword> <postgresqlDatabaseName> <hasuraAdminSecret> <jwtKeyAlgorithm> <hasuraClaimsNamespace> <jwtIssuer>"
-  exit 1
-fi
+cd "${0%/*}"
 
-BASE_URL=$1
-POSTGRESQL_RELEASE_NAME=$2
-POSTGRESQL_NAMESPACE=$3
-IAM_RELEASE_NAME=$4
-IAM_NAMESPACE=$5
-POSTGRESQL_USERNAME=$6
-POSTGRESQL_PASSWORD=$7
-POSTGRESQL_DATABASE_NAME=$8
-HASURA_ADMIN_SECRET=$9
-JWT_KEY_ALGORITHM=${10}
-HASURA_CLAIMS_NAMESPACE=${11}
-JWT_ISSUER=${12}
+IAM_RELEASE_NAME=fusionauth
+IAM_NAMESPACE=iam
+POSTGRESQL_USERNAME=hasura_user
+POSTGRESQL_PASSWORD=GfNU3ZLHeHu2Ic3Ioo3hOoLd8Mmm7oJeGmCxKZkmv1RBUA8qJcGmYdzTewjmIuj8
+POSTGRESQL_DATABASE_NAME=hasura
+HASURA_ADMIN_SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
+JWT_KEY_ALGORITHM=RS256
+HASURA_CLAIMS_NAMESPACE=https://hasura.io/jwt/claims
+JWT_ISSUER=jelasticozor.com
 
-POSTGRESQL_HOSTNAME=${POSTGRESQL_RELEASE_NAME}-primary.${POSTGRESQL_NAMESPACE}
-POSTGRESQL_PORT=$(kubectl -n ${POSTGRESQL_NAMESPACE} get svc ${POSTGRESQL_RELEASE_NAME} -o jsonpath='{.spec.ports[?(@.protocol=="TCP")].port}')
+POSTGRESQL_HOSTNAME=node155513-jelasticozor-db.hidora.com
+POSTGRESQL_PORT=5432
 IAM_HOSTNAME=${IAM_RELEASE_NAME}.${IAM_NAMESPACE}
-IAM_PORT=$(kubectl -n ${IAM_NAMESPACE} get svc ${IAM_RELEASE_NAME} -o jsonpath='{.spec.ports[?(@.protocol=="TCP")].port}')
+IAM_PORT=9011
 
-helm repo add platyplus https://charts.platy.plus
-helm repo update
+#helm repo add nexus https://%system.package-manager.deployer.username%:%system.package-manager.deployer.password%@%system.package-manager.hostname%
+#helm repo update
 
 # TODO: we probably want to download another set of values in the production case
-
-HASURA_VALUES=/tmp/hasura-values.yaml
-
-wget "${BASE_URL}/hasura/hasura-values.yaml" -O ${HASURA_VALUES}
+HASURA_VALUES=values.yaml
 
 sed -i s@POSTGRESQL_HOSTNAME@${POSTGRESQL_HOSTNAME}@g ${HASURA_VALUES}
 sed -i s/POSTGRESQL_PORT/${POSTGRESQL_PORT}/g ${HASURA_VALUES}
@@ -47,5 +38,5 @@ sed -i s/JWT_ISSUER/${JWT_ISSUER}/g ${HASURA_VALUES}
 NAMESPACE="api"
 RELEASE_NAME="hasura"
 
-helm upgrade --install --create-namespace --namespace ${NAMESPACE} ${RELEASE_NAME} platyplus/hasura \
-  -f ${HASURA_VALUES}
+helm upgrade --install --create-namespace --namespace ${NAMESPACE} ${RELEASE_NAME} nexus/hasura \
+  -f values.yaml
